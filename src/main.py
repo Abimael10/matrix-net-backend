34475d7abb5440b566f229a469be868cb8c1978f
Abimael10 <juanabimael@gmail.com>
2025-08-01 14:55:20 -0400

    Adding the HTTP exceptions logs to be handled effectively

--- src/main.py
+++ src/main.py
@@ -1,10 +1,15 @@
+import logging
 from contextlib import asynccontextmanager
 
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+
+from fastapi.exception_handlers import http_exception_handler
 
 from src.db import database
 from src.log_config import configure_logging
 from src.routers.post import router as post_router
+
+logger = logging.getLogger(__name__)
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -16,6 +21,11 @@ async def lifespan(app: FastAPI):
 app = FastAPI(lifespan=lifespan)
 
 app.include_router(post_router)
+
+@app.exception_handler(HTTPException)
+async def http_exception_handle_logging(request, exc):
+    logger.error(f"HTTPException: {exc.status_code} {exc.detail}")
+    return await http_exception_handler(request, exc)
 
 @app.get("/")
 async def root():
