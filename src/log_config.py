75c9bbd5e8767dbda510266243133574d9420472
Abimael10 <juanabimael@gmail.com>
2025-08-01 14:30:35 -0400

    adding the handlers to manage the log files

--- src/log_config.py
+++ src/log_config.py
@@ -12,6 +12,11 @@ def configure_logging() -> None:
                     "class": "logging.Formatter",
                     "datefmt": "%Y-%m-%dT%H:%M:%S",
                     "format": "%(name)s:%(lineno)d - %(message)s",
+                },
+                "file": {
+                    "class": "logging.Formatter",
+                    "datefmt": "%Y-%m-%dT%H:%M:%S",
+                    "format": "%(asctime)s.%(msecs)03dZ | %(levelname)-8s | %(name)s:%(lineno)d - %(message)s"
                 }
             },
             "handlers": {
@@ -19,15 +24,24 @@ def configure_logging() -> None:
                     "class": "rich.logging.RichHandler",
                     "level": "DEBUG",
                     "formatter": "console",
+                },
+                "rotating_file": {
+                    "class": "logging.handlers.RotatingFileHandler",
+                    "level": "DEBUG",
+                    "formatter": "file",
+                    "filename": "storeapi.log",
+                    "maxBytes": 1024 * 1024 * 5,  # 5MB
+                    "backupCount": 5,
+                    "encoding": "utf8"
                 }
             },
             "loggers": {
                 "src": {
-                    "handlers": ["default"],
+                    "handlers": ["default", "rotating_file"],
                     "level": "DEBUG" if isinstance(config, DevConfig) else "INFO",
                     "propagate": False
                 },
-                "databases": {"handlers": ["default"], "level": "WARNING"},
+                "databases": {"handlers": ["default", "rotating_file"], "level": "WARNING"},
                 #Removing when I migrate to PostgreSQL
                 "aiosqlite": {"handlers": ["default"], "level": "WARNING"}
             },
