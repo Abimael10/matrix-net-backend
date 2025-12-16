3441bf209b3c23e35aef61a859dfddab05ce405b
abimael10 <juanabimael@gmail.com>
2025-12-15 21:09:15 -0400

    adding a get comment function to get a single comment for this view

--- src/views/comments.py
+++ src/views/comments.py
@@ -7,3 +7,8 @@ from src.db import comment_table, database
 async def list_comments_for_post(post_id: int):
     query = comment_table.select().where(comment_table.c.post_id == post_id)
     return await database.fetch_all(query)
+
+
+async def get_comment(comment_id: int):
+    query = comment_table.select().where(comment_table.c.id == comment_id)
+    return await database.fetch_one(query)
